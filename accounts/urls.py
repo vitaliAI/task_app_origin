7386438f75a8a306a0c8d735c68e0ea0abd331d1
vitaliAI <vitali_mueller@yahoo.de>
2018-10-30 23:35:54 +0000

    created urls and views for accounts

---
+++ accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from accounts import views
+
+
+app_name = 'auth'
+
+urlpatterns = [
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('register/', views.register, name='register'),
+]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,46 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from accounts.forms import LoginForm, RegistrationForm
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST or None)
+        if form.is_valid():
+            user = authenticate(
+                username=request.POST.get('username'),
+                password=request.POST.get('password')
+            )
+
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    # Vitali in Development -> Redirect to Home View
+                    return redirect('home')
+            else:
+                return render(request, 'accounts/login.html', {'form': form})
+    else:
+        return render(request, 'accounts/login.html', {'form': LoginForm})
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST or None)
+        if form.is_valid():
+            User.objects.create_user(
+                username=request.POST.get('username'),
+                email=request.POST.get('email'),
+                password=request.POST.get('password')
+            )
+            return redirect('auth:login')
+        else:
+            return render(request, 'accounts/register.html', {'form': form})
+    else:
+        return render(request, 'accounts/register.html', {'form': RegistrationForm()})
+
+
+def logout_view(request):
+    logout(request)
+    # Vitali in Development -> Redirect to Home View
+    return redirect('home')
